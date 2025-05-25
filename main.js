e21d417a924f84bbf329f2871e74884aa33fb68c
Eggloit <ellfri248@edu.linkoping.se>
2025-05-25 21:30:18 +0000

    klhghjgkrdö

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
+const hamburgare = document.querySelector(".hamburgare");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
+hamburgare.addEventListener("click", () => {
+  hamburgare.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
 document
   .querySelectorAll(".nav-link")
   .forEach((n) => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
+    hamburgare.classList.remove("active");
     navMenu.classList.remove("active");
   }));
